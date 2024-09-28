de6c7d5e9ade676c7d508a10260ba61e97c5fdb8
LianJordaan <lianjordaan30052005@gmail.com>
2024-09-28 02:52:17 +0200

    Removed unused code.

--- src/main/java/io/github/lianjordaan/byteBuildersLobbyPlugin/events/AsyncChat.java
+++ src/main/java/io/github/lianjordaan/byteBuildersLobbyPlugin/events/AsyncChat.java
@@ -5,13 +5,16 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.github.lianjordaan.byteBuildersLobbyPlugin.ByteBuildersLobbyPlugin;
 import io.github.lianjordaan.byteBuildersLobbyPlugin.utils.LuckpermsUtils;
+import io.github.lianjordaan.byteBuildersLobbyPlugin.utils.SoundUtils;
 import io.github.lianjordaan.byteBuildersLobbyPlugin.utils.WebSocketUtils;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -22,11 +25,10 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class AsyncChat implements Listener {
 
-    private final Logger logger = ByteBuildersLobbyPlugin.getPlugin(ByteBuildersLobbyPlugin.class).getLogger();
+    private final Logger logger = ByteBuildersLobbyPlugin.getPlugin(ByteBuildersLobbyPlugin.class).getSLF4JLogger();
     protected final Server server = ByteBuildersLobbyPlugin.getPlugin(ByteBuildersLobbyPlugin.class).getServer();
 
     @EventHandler
@@ -41,16 +43,6 @@ public class AsyncChat implements Listener {
 //                event.setResult(PlayerChatEvent.ChatResult.denied()); // Prevent the command from being shown in chat
             } else {
                 event.getPlayer().sendMessage(Component.text("Invalid command usage. Use start <port>"));
-            }
-        }
-        else if (message.startsWith("create")) {
-            String[] parts = message.split(" ");
-            if (parts.length == 2) {
-                String size = parts[1];
-                handleCreateCommand(event.getPlayer(), size);
-                event.getPlayer().sendMessage(Component.text("Creating plot... Please wait."));
-            } else {
-                event.getPlayer().sendMessage(Component.text("Invalid command usage. Use create"));
             }
         }
         else if (message.startsWith("delete")) {
@@ -82,7 +74,7 @@ public class AsyncChat implements Listener {
                 // Read response based on the status code
                 try (Scanner scanner = new Scanner(responseCode >= 400 ? connection.getErrorStream() : connection.getInputStream())) {
                     String response = scanner.useDelimiter("\\A").next();
-                    logger.log(Level.INFO, "Server start response: {}", response);
+                    logger.info("Server start response: {}", response);
                     JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
 
                     if (responseCode == 200) {
@@ -101,59 +93,11 @@ public class AsyncChat implements Listener {
                     } else if (responseCode == 410) { // New response code for plot not found
                         server.getPlayer(player.getUniqueId()).sendMessage(Component.text("The plot ID does not exist."));
                     } else {
-                        logger.log(Level.SEVERE, "Failed to start the server with response code: {}", responseCode);
+                        logger.error("Failed to start the server with response code: {}", responseCode);
                     }
                 }
             } catch (IOException e) {
-                logger.log(Level.SEVERE, "Error starting the server", e);
-            }
-        });
-    }
-
-    private void handleCreateCommand(Player player, String size) {
-        // Start the server by making a web request
-        CompletableFuture.runAsync(() -> {
-            try {
-                // Make HTTP request to start the server
-                URL url = new URL("http://localhost:3000/create-plot");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setDoOutput(true);
-
-                // Create a Map to hold the JSON data
-                Map<String, Object> jsonMap = new HashMap<>();
-                jsonMap.put("name", "<white>" + player.getName() + "'s Plot");
-                jsonMap.put("description", "<white>Default description of " + player.getName() + "'s plot");
-                jsonMap.put("size", size);
-                jsonMap.put("ownerUuid", player.getUniqueId().toString());
-                jsonMap.put("rank", LuckpermsUtils.getHighestRank(player));
-
-                // Convert the Map to a JSON string using Gson
-                Gson gson = new Gson();
-                String jsonInputString = gson.toJson(jsonMap);
-
-                connection.getOutputStream().write(jsonInputString.getBytes(StandardCharsets.UTF_8));
-                int responseCode = connection.getResponseCode();
-
-                // Read response based on the status code
-                try (Scanner scanner = new Scanner(responseCode >= 400 ? connection.getErrorStream() : connection.getInputStream())) {
-                    String response = scanner.useDelimiter("\\A").next();
-                    logger.log(Level.INFO, "Server start response: {}", response);
-                    JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
-
-                    if (responseCode == 201) {
-                        player.sendMessage(Component.text("Your plot has been created. ID: " + jsonResponse.get("id").getAsString()));
-                    } else if (responseCode == 400) {
-                        player.sendMessage(Component.text("Error: " + jsonResponse.get("message").getAsString()));
-                    } else if (responseCode == 500) {
-                        server.sendMessage(Component.text("Server Error: Failed to create plot. Please try again later."));
-                    } else {
-                        logger.log(Level.SEVERE, "Failed to start the server with response code: {}", responseCode);
-                    }
-                }
-            } catch (IOException e) {
-                logger.log(Level.SEVERE, "Error starting the server", e);
+                logger.error("Error starting the server", e);
             }
         });
     }
@@ -178,7 +122,7 @@ public class AsyncChat implements Listener {
                 // Read response based on the status code
                 try (Scanner scanner = new Scanner(responseCode >= 400 ? connection.getErrorStream() : connection.getInputStream())) {
                     String response = scanner.useDelimiter("\\A").next();
-                    logger.log(Level.INFO, "Plot delete response: {}", response);
+                    logger.info("Plot delete response: {}", response);
                     JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
 
                     if (responseCode == 200) {
@@ -191,11 +135,11 @@ public class AsyncChat implements Listener {
                         player.sendMessage(Component.text("Error: Plot with ID " + id + " is running. Please stop the server before deleting."));
                     } else {
                         player.sendMessage(Component.text("Error: Failed to delete plot. Please try again later."));
-                        logger.log(Level.SEVERE, "Failed to delete the plot with response code: {}", responseCode);
+                        logger.error("Failed to delete the plot with response code: {}", responseCode);
                     }
                 }
             } catch (IOException e) {
-                logger.log(Level.SEVERE, "Error deleting the plot", e);
+                logger.error("Error deleting the plot", e);
             }
         });
     }
